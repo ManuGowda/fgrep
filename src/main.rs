174af032a0ced662e0a0c3260010eb3182560f4b
Manu Nelamane Siddalingegowda <manu@lightcurve.io>
2019-08-07 22:10:38 +0200

    :seedling: Implement grep command using rust

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use std::env;
+use std::process;
+
+use fgrep::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    // here the error handled using unwrap to collect the result
+    let config = Config::new(&args).unwrap_or_else(|error| {
+        eprintln!("Problem parsing arguments: {}", error);
+        process::exit(1);
+    });
+
+    println!("Searching for: {}", config.query);
+    println!("In file: {}", config.filename);
+
+    // here if let is used since we only need to worry about error case
+    if let Err(e) = fgrep::run(config) {
+        eprintln!("Application error: {}", e);
+
+        process::exit(1);
+    }
+}
